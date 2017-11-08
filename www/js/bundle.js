799b744cdb45ff5d7572dd98404664ea8aa9a2b7
sg-mamedova <sabina.mamedova90@gmail.com>
2017-11-08 11:44:39 +0200

    www

---
+++ www/js/bundle.js
@@ -0,0 +1,27 @@
+
+$(function() {
+    $('#carousel ul').slick({
+        dots: true,
+        nextArrow: '<a id="next" href="#" class="next"><div class="i-box"><i class="icon i-arrow"></i></div></a>'
+    });
+
+    //$('#carousel ul').carouFredSel({
+    //    width: '100%',
+    //    items: {
+    //        visible: 1,
+    //        start: -1
+    //    },
+    //    scroll: {
+    //        items: 1,
+    //        duration: 1500,
+    //        timeoutDuration: 4000
+    //    },
+    //    pagination: "#pager",
+    //    auto: true,
+    //    swipe: { onTouch: true, onMouse: true },
+    //    pauseOnHover: true,
+    //    align: 'center',
+    //    responsive: true,
+    //    next: '#next'
+    //});
+});
